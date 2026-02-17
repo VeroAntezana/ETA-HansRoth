0fd61cd33d2aee361568f6fae10a9de7b83040a6
SalvatierraJ <javiersalvatierra44@gmail.com>
2026-02-17 18:40:19 -0400

    cambio parcial en busqueda

--- resources/views/estudiantes/create.blade.php
+++ resources/views/estudiantes/create.blade.php
@@ -198,7 +198,7 @@
 
         const buscarEstudiante = async (texto) => {
             try {
-                const url = `{{ config('app.url') }}/api/estudiantes/create/buscar?q=${texto}`;
+                const url = `{{config('app.url')}}/api/estudiantes/create/buscar?q=${texto}`;
                 const resultado = await fetch(url);
                 if (!resultado.ok) {
                     throw new Error(`Error en la solicitud: ${resultado.status}`);
